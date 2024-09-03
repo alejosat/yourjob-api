c531b6caa31b20752eef5a432263ed9544bb76f9
Alejandro Santis <asantiscastro@gmail.com>
2024-09-02 21:16:25 -0500

    Agregando los metodos al controlador JobSeekerProfileController y sus formRequest

--- app/Http/Controllers/Api/JobSeekerProfileController.php
+++ app/Http/Controllers/Api/JobSeekerProfileController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreJobSeekerProfileRequest;
+use App\Http\Requests\UpdateJobSeekerProfileRequest;
 use App\Models\JobSeekerProfile;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -21,32 +24,51 @@ class JobSeekerProfileController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreJobSeekerProfileRequest $request): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        $profile = $request->user()->jobSeekerProfile()->create($validated);
+
+        return response()->json([
+            'message' => 'Profile created successfully',
+            'data' => $profile,
+
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(JobSeekerProfile $jobSeekerProfile): JsonResponse
     {
-        //
+        $jobSeekerProfile->load('user');
+        return response()->json($jobSeekerProfile, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateJobSeekerProfileRequest $request, JobSeekerProfile $jobSeekerProfile): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        $jobSeekerProfile->update($validated);
+
+        return response()->json([
+            'message' => 'Profile updated successfully',
+            'data' => $jobSeekerProfile,
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(JobSeekerProfile $jobSeekerProfile): JsonResponse
     {
-        //
+        $jobSeekerProfile->delete();
+
+        return response()->json([
+            'message' => 'Profile deleted successfully',
+            'data' => null,
+        ], 204);
     }
 }
